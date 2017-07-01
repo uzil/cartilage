73f5eec21c560e63e672c4b511ca316a3f3333f8
ujjwal <ujjwal25993@live.com>
2017-07-01 19:08:54 +0530

    bugfix(validation): fix allowUnknown always true for all keys
    close #1

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var Promise = require('bluebird');
 
 function validate(method, candidate, schema, options) {
   return new Promise(function (resolve, reject) {
+    if (method === 'headers') schema = joi.compile(schema).unknown();
+
     joi.validate(candidate, schema, options, function (error, value) {
       if (error) return reject(error);
       resolve({ method: method, value: value });
@@ -14,7 +16,6 @@ function validationMiddleware(schema, joiOptions) {
   return function (req, res, next) {
     var options = joiOptions || {};
     var operations = Object.keys(schema).reduce(function (acc, method) {
-      if (method === 'headers') options.allowUnknown = true;
       if (req[method]) acc.push(validate(method, req[method], schema[method], options));
       return acc;
     }, []);
