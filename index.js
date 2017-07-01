e0ba20f0a48f295f32f435cbc0ca40cd9c58278b
ujjwal <ujjwal25993@live.com>
2017-07-01 15:29:53 +0530

    Inital commit

---
+++ index.js
@@ -0,0 +1,34 @@
+var joi = require('joi');
+var Promise = require('bluebird');
+
+function validate(method, candidate, schema, options) {
+  return new Promise(function (resolve, reject) {
+    joi.validate(candidate, schema, options, function (error, value) {
+      if (error) return reject(error);
+      resolve({ method: method, value: value });
+    });
+  });
+}
+
+function validationMiddleware(schema, joiOptions) {
+  return function (req, res, next) {
+    var options = joiOptions || {};
+    var operations = Object.keys(schema).reduce(function (acc, method) {
+      if (method === 'headers') options.allowUnknown = true;
+      if (req[method]) acc.push(validate(method, req[method], schema[method], options));
+      return acc;
+    }, []);
+
+    Promise.all(operations)
+      .then(function (results) {
+        results.forEach(function (result) {
+          req[result.method] = result.value;
+        });
+
+        next();
+      })
+      .catch(next);
+  }
+}
+
+module.exports = validationMiddleware;
